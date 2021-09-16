c38ba211ff09170361a8788a2c3147f588b0dc59
kakashimaker <87550718+K4K4SH1M4K3R@users.noreply.github.com>
2021-09-15 21:28:25 -0300

    Create Script.py

---
+++ Script.py
@@ -0,0 +1,10 @@
+
+import os
+
+print("olaaaaa amigos: ")
+
+print("https://github.com/Zian25/UniTools-Termux")   
+
+print("https://jornadadodev.com.br/cursos")
+
+print("escolham e colam na barra de pesquisa do google: ")
