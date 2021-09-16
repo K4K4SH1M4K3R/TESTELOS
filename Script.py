4c572745b32564552d3d322bf1337b0adc41ef37
kakashimaker <87550718+K4K4SH1M4K3R@users.noreply.github.com>
2021-09-15 21:14:57 -0300

    Create Script.py

---
+++ Script.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import sys
+if sys.version_info[0] < 3:
+    print ("Python 2 não é suportado, mn tenta o python3 :)")
+    exit(0)
+    
+
+import os
+
+print("olaaaaa amigos: ")
+
+1 - ('https://github.com/Zian25/UniTools-Termux')   
+
+1 - ('https://jornadadodev.com.br/cursos')
+
+print("escolham e colam na barra de pesquisa do google: ")
