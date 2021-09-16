a2045e8ee0c80793c66c845078c0a005b13e6e13
kakashimaker <87550718+K4K4SH1M4K3R@users.noreply.github.com>
2021-09-15 21:27:28 -0300

    Delete Script.py

--- Script.py
+++
@@ -1,9 +0,0 @@
-import os
-
-print("olaaaaa amigos: ")
-
-print("https://github.com/Zian25/UniTools-Termux")   
-
-print("https://jornadadodev.com.br/cursos")
-
-print("escolham e colam na barra de pesquisa do google: ")
